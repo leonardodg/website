c0ba838cc049b433ec598fee96ed94144f3ffba0
LeoDG <leonardodg@msn.com>
2025-06-04 16:30:12 +0000

    install eslint and config

---
+++ src/js/index.js
@@ -0,0 +1,17 @@
+const btn = document.getElementById("user-menu-button");
+const menu = document.getElementById("user-menu-dropdown");
+
+const hello = "Hello, World!";
+
+btn.addEventListener("click", function (e) {
+  e.stopPropagation();
+  menu.classList.toggle("hidden");
+});
+
+document.addEventListener("click", function () {
+  if (!menu.classList.contains("hidden")) {
+    menu.classList.add("hidden");
+  }
+});
+
+console.log(hello);
